0fd30c974f2e513317a733123d663dc614e64a29
bengris32 <bengris32@protonmail.ch>
2024-10-07 23:50:11 +0100

    sm8450-common: camera: Implement supportsSetTorchModeExt
    Change-Id: I2ec2f4a30723763e6123a1b742468752f38e3d2f
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -44,6 +44,10 @@ bool supportsTorchStrengthControlExt() {
     return true;
 }
 
+bool supportsSetTorchModeExt() {
+    return false;
+}
+
 int32_t getTorchDefaultStrengthLevelExt() {
     return 7;
 }
